76faedd0cc8bba68a6ef13c2070b12c1932e85d6
Taylor Sieling <taylorasieling@gmail.com>
2020-08-26 21:18:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,18 +6,6 @@ def square_array(array)
     new_array << integer ** 2
   end
   new_array
-  end
-  squared_numbers
 end
-
+  
 square_array(numbers)
-
-def square_array(array)
-
- new_array = []
-
- array.each do |integer|
-  new_array << integer ** 2
- end
- return new_array
-end
