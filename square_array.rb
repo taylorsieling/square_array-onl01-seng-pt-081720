5d5e9dd873e103eb325fd83a3623c972208e9248
Taylor Sieling <taylorasieling@gmail.com>
2020-08-26 21:09:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,11 @@
 numbers = [1,2,3]
 
 def square_array(numbers)
+  squared_numbers = []
   numbers.each do |number|
-    #{number}**2
+  squared_numbers <<#{number}**2
   end
+  squared_numbers
 end
 
 square_array(numbers)
